532d4e3ca4bc6d0d42061dc9261a070fd869b96c
Galse22 <al.g.se622@gmail.com>
2021-04-14 19:58:37 -0300

    Players can now place fake turrets
    Players can now place fake Turrets on the TurretsSpots + Object Pool system + Turrets Scriptable Object

---
+++ BW Jam/Assets/Scripts/Player/TurretManagerPlayerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretManagerPlayerScript : MonoBehaviour
+{
+    public GameObject currentSpot;
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "TurretSpot")
+        {
+            if(col.transform.childCount == 0)
+            {
+                currentSpot = col.gameObject;
+            }
+        }
+        if(currentSpot != null)
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                GameObject turret1 = ObjectPooler.SharedInstance.GetPooledObject("Turret1");
+                turret1.transform.SetParent(currentSpot.transform);
+                currentSpot = null;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "TurretSpot")
+        {
+            if(col.transform.childCount == 0)
+            {
+                currentSpot = col.gameObject;
+            }
+        }
+        if(currentSpot != null)
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                GameObject turret = ObjectPooler.SharedInstance.GetPooledObject("Turret1");
+                if (turret != null) {
+                    turret.transform.position = currentSpot.transform.position;
+                    turret.transform.rotation = currentSpot.transform.rotation;
+                    turret.SetActive(true);
+                    turret.transform.SetParent(currentSpot.transform);
+                    currentSpot = null;
+                }
+            }
+        }
+    }
+}
---
+++ BW Jam/Assets/Scripts/ScriptableObject/TurretUIScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TurretUIScriptableObject", menuName = "BW Jam/TurretUIScriptableObject", order = 0)]
+public class TurretUIScriptableObject : ScriptableObject 
+{
+    public GameObject turretToPool;
+    public string uiThingyNameToPool;
+
+    public float moneyThatCosts;
+}
